bb0070a8fdbfc1997b5189ac576ee02971ab12cd
Álvaro Rivas <alvarorivasg26@gmail.com>
2019-12-01 22:34:32 +0100

    api solved

--- api.py
+++ api.py
@@ -1,50 +1,107 @@
 from bottle import route, run, get, post, request
 import random
 from organizemongo import connectCollection
-import bson
+from bson.json_util import dumps
 from pymongo import MongoClient
 import getpass
+from datetime import datetime
 
 
 password = getpass.getpass("Insert your AtlasMongoDB alvaro password: ")
-connection = "mongodb+srv://alvaro:{}@apiproject-6gblk.mongodb.net/test?retryWrites=true&w=majority".format(password)
-client= MongoClient(connection)
-db, messages = connectCollection('chatNLPapi','messages',client)
-_, users = connectCollection('chatNLPapi','users',client)
-_, chats = connectCollection('chatNLPapi','chats',client)
+connection = "mongodb+srv://alvaro:{}@apiproject-6gblk.mongodb.net/test?retryWrites=true&w=majority".format(
+    password)
+client = MongoClient(connection)
+db, messages = connectCollection('chatNLPapi', 'messages', client)
+_, users = connectCollection('chatNLPapi', 'users', client)
+_, chats = connectCollection('chatNLPapi', 'chats', client)
 
 
-@get("/")
-def index():
-    return {
-        "nombre": random.choice(["Pepe", "Juan", "Fran", "Luis"])
+'''class CollConection:
+
+    def __init__(self,dbName,collection):
+        self.client = MongoClient()
+        self.db = self.client[dbName]
+        self.collection=self.db[collection]
+
+coll=CollConection('Prueba','datamad1019')'''
+
+
+@get("/prueba")
+def test():
+    return {"Connected"}
+
+
+@get("/chat/<idChat>")
+def getChat(idChat):
+    '''
+    idChat must be an integer
+    '''
+    chatid = list(chats.find({'idChat': int(idChat)}))[0]['_id']
+    datachat = list(messages.find({'idChat': chatid}))
+    rtrn = {}
+    for i in range(len(datachat)):
+        name = datachat[i]['userName']
+        message = datachat[i]['text']
+        datetime = datachat[i]['datetime']
+        rtrn[f'message {i+1}'] = name, message, datetime
+    return dumps(rtrn)
+
+
+@get("/users")
+def getUsers():
+    return dumps(users.find({}))
+
+
+@post('/create/user')
+def createUser():
+    '''Recibe parámetros de la siguiente estructura: {'name': 'Pepe'}'''
+    new_id = max(users.distinct("idUser")) + 1
+    name = request.forms["name"]
+    new_user = {
+        "idUser": new_id,
+        "userName": name
     }
+    return {"Succesfully inserted": str(users.insert_one(new_user).inserted_id)}
 
 
-@get("/chiste/<tipo>")
-def demo2(tipo):
-    print(f"un chiste de {tipo}")
-    if tipo == "chiquito":
-        return {
-            "chiste": "Van dos soldados en una moto y no se cae ninguno porque van soldados"
-        }
-    elif tipo == "eugenio":
-        return {
-            "chiste": "Saben aquell que diu...."
-        }
+@post('/create/chat')
+def createChat():
+    '''Recibe como parámetros un diccionario vacío'''
+    new_id = max(chats.distinct("idChat")) + 1
+    new_chat = {
+        "idChat": new_id,
+    }
+    return {"Succesfully inserted": str(chats.insert_one(new_chat).inserted_id)}
+
+
+@post('/create/message')
+def createMessage():
+    '''Recibe como parámetros la siguiente estructura:
+    {"userName":,"idChat":,"text":}
+    Nota: si el idChat introducido como parámetro no existe, se creará un nuevo idChat con un número
+    distinto al introducido. Ver createChat'''
+    user = list(users.find({'userName': request.forms['name']}))
+    if len(user) == 0:
+        createUser()
+        userid = list(users.find({'userName': request.forms['name']}))[
+            0]['_id']
     else:
-        return {
-            "chiste": "No puedorrr!!"
-        }
+        userid = user[0]['_id']
+    try:
+        chat = list(chats.find({'idChat': int(request.forms['idChat'])}))
+    except:
+        pass
+    if 'idChat' not in request.forms.keys() or len(chat) == 0:
+        createChat()
+        chatid = list(chats.find({'idChat': max(chats.distinct("idChat"))}))[0]['_id']
+    else:
+        chatid = chat[0]['_id']
+    rtrn = {'idUser': userid, 'userName': request.forms['name'],
+            'idMessage': max(messages.distinct('idMessage')) + 1,
+            'idChat': chatid,'datetime':datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'text': request.forms['text']}
+    messages.insert_one(rtrn)
+    return dumps(rtrn)
 
-@post('/add')
-def add():
-    print(dict(request.forms))
-    autor=request.forms["autor"]
-    chiste=request.forms.get("chiste")  
-    return {
-        "inserted_doc": str(coll.addChiste(autor,chiste))}
-
-
-#_, coll=creaclass('Prueba','datamad1019')esto solo era necesario si hago class
+# _, coll=creaclass('Prueba','datamad1019')esto solo era necesario si hago class
 run(host='0.0.0.0', port=8080)
